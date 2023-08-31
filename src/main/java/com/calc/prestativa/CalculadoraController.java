ecbc04f0448d90f2bde1d4ae98488ce678332d13
Henrique Strada <henriquestradax@gmail.com>
2023-08-30 21:55:20 -0300

    Dalee

--- src/main/java/com/calc/prestativa/CalculadoraController.java
+++ src/main/java/com/calc/prestativa/CalculadoraController.java
@@ -8,15 +8,76 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculadoraController {
 
     @GetMapping("/soma")
-    public int calcular(@RequestParam int valor1, @RequestParam int valor2) {
+    public double calcular(@RequestParam double valor1, @RequestParam double valor2) {
         // Realizar a soma dos valores
         System.out.println("Primeiro valor: " + valor1);
         System.out.println("Segundo valor: " + valor2);
-        int resultado = valor1 + valor2;
+        double resultado = valor1 + valor2;
         System.out.println("Resultado: " + resultado);
         return resultado;
     }
 
+    @GetMapping("/sub")
+    public double sub(@RequestParam double valor1, @RequestParam double valor2) {
+        // Realizar a subtração dos valores
+        System.out.println("Primeiro valor: " + valor1);
+        System.out.println("Segundo valor: " + valor2);
+        double resultado = valor1 - valor2;
+        System.out.println("Resultado: " + resultado);
+        return resultado;
+    }
 
+    @GetMapping("/multi")
+    public double multi(@RequestParam double valor1, @RequestParam double valor2) {
+        // Realizar a multiplicação dos valores
+        System.out.println("Primeiro valor: " + valor1);
+        System.out.println("Segundo valor: " + valor2);
+        double resultado = valor1 * valor2;
+        System.out.println("Resultado: " + resultado);
+        return resultado;
+    }
+
+    @GetMapping("/div")
+    public double div(@RequestParam double valor1, @RequestParam double valor2) {
+        // Realizar a divisão dos valores
+        System.out.println("Primeiro valor: " + valor1);
+        System.out.println("Segundo valor: " + valor2);
+        double resultado = valor1 / valor2;
+        System.out.println("Resultado: " + resultado);
+        return resultado;
+    }
+    @GetMapping("/raiz")
+    public double raiz(@RequestParam double valor1) {
+        // Calcular a raiz quadrada do valor
+        System.out.println("Primeiro valor: " + valor1);
+        double resultado = Math.sqrt(valor1);
+        System.out.println("Resultado: " + resultado);
+        return resultado;
+    }
+    @GetMapping("/circu")
+    public double circu(@RequestParam double valor1) {
+            double resultado;
+            double raioDim = valor1 * 2;
+            resultado = raioDim * Math.PI;
+        System.out.println("Resultado: " + resultado);
+        return resultado;
+    }
+    @GetMapping("/hipo")
+    public double hipo(@RequestParam double valor1, @RequestParam double valor2) {
+        double resultado;
+        double raioDim = valor1 * 2;
+        resultado = raioDim * Math.PI;
+        System.out.println("Resultado: " + resultado);
+        return resultado;
+
+        /*
+        String valorCateto = JOptionPane.showInputDialog("Digite o valor do primeiro cateto de seu triangulo");
+        double catetoUm = Double.parseDouble(valorCateto);
+        String valorCatetoDois = JOptionPane.showInputDialog("Digite o valor do segundo cateto de seu triangulo");
+        double catetoDois = Double.parseDouble(valorCatetoDois);
+        double somCatQuard = (catetoUm * catetoUm) + (catetoDois * catetoDois);
+        JOptionPane.showMessageDialog(null, "A hipotenusa do seu triangulo é: " + Math.sqrt(somCatQuard));
+    */
+    }
 
 }
--- src/main/resources/static/dalejs.js
+++ src/main/resources/static/dalejs.js
@@ -28,7 +28,73 @@ function calcularSub() {
         .then(response => response.text())
         .then(resultado => {
             // Receber a resposta do servidor e exibir um alerta com o resultado
-            window.alert("O resultado da soma é: " + resultado);
+            window.alert("O resultado da subtração é: " + resultado);
+        });
+}
+
+function calcularMulti() {
+    // Obter os valores usando prompts
+    var valor1 = prompt("Digite o primeiro valor: ");
+    var valor2 = prompt("Digite o segundo valor: ");
+
+    // Enviar uma solicitação HTTP para o servidor com os valores
+    fetch('/multi?valor1=' + valor1 + '&valor2=' + valor2)
+        .then(response => response.text())
+        .then(resultado => {
+            // Receber a resposta do servidor e exibir um alerta com o resultado
+            window.alert("O resultado da multiplicação é: " + resultado);
+        });
+}
+
+function calcularDiv() {
+    // Obter os valores usando prompts
+    var valor1 = prompt("Digite o primeiro valor: ");
+    var valor2 = prompt("Digite o segundo valor: ");
+
+    // Enviar uma solicitação HTTP para o servidor com os valores
+    fetch('/div?valor1=' + valor1 + '&valor2=' + valor2)
+        .then(response => response.text())
+        .then(resultado => {
+            // Receber a resposta do servidor e exibir um alerta com o resultado
+            window.alert("O resultado da divisão é: " + resultado);
+        });
+}
+
+function calcularRaiz() {
+    // Obter os valores usando prompts
+    var valor1 = prompt("Digite o valor que deseja descobrir a raiz: ");
+
+    // Enviar uma solicitação HTTP para o servidor com os valores
+    fetch('/raiz?valor1=' + valor1)
+        .then(response => response.text())
+        .then(resultado => {
+            // Receber a resposta do servidor e exibir um alerta com o resultado
+            window.alert("O resultado da raiz é: " + resultado);
+        });
+}
+function calcularCircu() {
+    // Obter os valores usando prompts
+    var valor1 = prompt("Digite o valor do seu raio (caso tiver apenas diametro, divida por 2 e coloque aqui novamente):");
+
+    // Enviar uma solicitação HTTP para o servidor com os valores
+    fetch('/circu?valor1=' + valor1)
+        .then(response => response.text())
+        .then(resultado => {
+            // Receber a resposta do servidor e exibir um alerta com o resultado
+            window.alert("A circunferencia é: " + resultado);
+        });
+}
+function calcularHipo() {
+    // Obter os valores usando prompts
+    var valor1 = prompt("Digite o primeiro cateto: ");
+    var valor2 = prompt("Digite o segundo segundo: ");
+
+    // Enviar uma solicitação HTTP para o servidor com os valores
+    fetch('/Hipo?valor1=' + valor1 + '&valor2=' + valor2)
+        .then(response => response.text())
+        .then(resultado => {
+            // Receber a resposta do servidor e exibir um alerta com o resultado
+            window.alert("A hipotenusa é: " + resultado);
         });
 }
 
@@ -36,19 +102,21 @@ function calcularSub() {
 document.getElementById("soma").onclick = function() {
     calcularSoma();
 }
-
 document.getElementById("sub").onclick = function() {
    calcularSub();
 }
 document.getElementById("multi").onclick = function() {
-    var valores = pegaValores();
-    var resultado = parseFloat(valores[0]) * parseFloat(valores[1]);
-    window.alert("O resultado da multiplicação é: " + resultado);
-    console.log("O resultado da multiplicação é: " + resultado);
+    calcularMulti();
 }
 document.getElementById("div").onclick = function() {
-    var valores = pegaValores();
-    var resultado = parseFloat(valores[0]) / parseFloat(valores[1]);
-    window.alert("O resultado da divisão é: " + resultado);
-    console.log("O resultado da divisão é: " + resultado);
+    calcularDiv();
+}
+document.getElementById("raiz").onclick = function() {
+    calcularRaiz();
+}
+document.getElementById("circu").onclick = function() {
+    calcularCircu();
+}
+document.getElementById("hipo").onclick = function() {
+    calcularHipo();
 }
