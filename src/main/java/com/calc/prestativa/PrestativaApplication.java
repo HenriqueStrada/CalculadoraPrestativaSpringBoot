8d7790a542adb8bcb58abce44d995b7998b4c904
Henrique Strada <henriquestradax@gmail.com>
2023-10-02 19:22:11 -0300

    Testando commit

--- src/main/java/com/calc/prestativa/PrestativaApplication.java
+++ src/main/java/com/calc/prestativa/PrestativaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrestativaApplication {
 
-
+//testando commit
 	public static void main(String[] args) {
 		SpringApplication.run(PrestativaApplication.class, args);
 	}
