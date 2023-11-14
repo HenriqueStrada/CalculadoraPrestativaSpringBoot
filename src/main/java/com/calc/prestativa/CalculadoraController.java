a74516ca39e458df2bb0c02bfdcce868a6921c51
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-11-13 21:32:23 -0300

    Styleeeeee

--- src/main/java/com/calc/prestativa/CalculadoraController.java
+++ src/main/java/com/calc/prestativa/CalculadoraController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
+//
 @RestController
 public class CalculadoraController {
     @GetMapping("/logCad")
