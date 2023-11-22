e4c2a1371b9de63934e7ebe7b0338418946cfd52
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-11-22 19:25:11 -0300

    AAbbAACC

--- src/main/java/com/calc/prestativa/regrasSpringSecurity/SecurityConfigurations.java
+++ src/main/java/com/calc/prestativa/regrasSpringSecurity/SecurityConfigurations.java
@@ -29,7 +29,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
