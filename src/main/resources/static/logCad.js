3e97aae01a4ba81a63385c442e9c5d4d8f66f0c1
Henrique Strada <henriquestradax@gmail.com>
2023-12-01 22:00:08 -0300

    alterações de index, formatação, e adicionamento de tela de deletar usuario

--- src/main/resources/static/logCad.js
+++ src/main/resources/static/logCad.js
@@ -1,15 +1,5 @@
 window.onload = function() {
     $(document).ready(function() {
-        $("#userAlt").click(function() {
-            // Esconde o formulário 1
-            $('#form1').hide();
-            $("#userVoltar").show();
-            // Exibe o formulário 2
-            $('.form2').show();
-            $("#userAlt").hide();
-        });
-    });
-    $(document).ready(function() {
         $("#userVoltar").click(function() {
             //Mostra o formulário 1
             $('#form1').show();
@@ -17,6 +7,35 @@ window.onload = function() {
             //Esconde o formulário 2
             $('.form2').hide();
             $("#userAlt").show();
+            // Esconde o formulário 3
+            $(".form3").hide();
+            $("#userDelet").show();
+        });
+    });
+    $(document).ready(function() {
+        $("#userAlt").click(function() {
+            // Esconde o formulário 1
+            $('#form1').hide();
+            $("#userVoltar").show();
+            // Exibe o formulário 2
+            $('.form2').show();
+            $("#userAlt").hide();
+            // Esconde o formulário 3
+            $(".form3").hide();
+            $("#userDelet").hide();
+        });
+    });
+    $(document).ready(function() {
+        $("#userDelet").click(function() {
+            //Esconde o formulário 1
+            $('#form1').hide();
+            $("#userVoltar").show();
+            //Esconde o formulário 2
+            $('.form2').hide();
+            $("#userAlt").hide();
+            // Mostrar o formulário 3
+            $(".form3").show();
+            $("#userDelet").hide();
         });
     });
 };
